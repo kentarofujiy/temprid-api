2d49ea176d87e616a6023bf4ca360df06d17bd9c
Kentaro Fujiy <kentaro@manacadigital.com.br>
2018-04-22 04:49:10 +0000

    search

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -2,5 +2,9 @@ class Poi < ApplicationRecord
       has_one_attached :avatar
       geocoded_by :address
       after_validation :geocode
-      searchkick
+      searchkick locations: [:location]
+
+      def search_data
+        attributes.merge location: { lat: latitude, lon: longitude }    
+      end
 end
